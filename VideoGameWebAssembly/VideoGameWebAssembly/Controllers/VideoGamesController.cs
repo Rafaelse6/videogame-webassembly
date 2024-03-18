1784c30c3f04d24381fb7fd5c903760b2503f68f
Rafael Santos <rafa.05125@gmail.com>
2024-03-18 00:12:11 -0300

    Implementing CRUD in the API Controller

--- VideoGameWebAssembly/VideoGameWebAssembly/Controllers/VideoGamesController.cs
+++ VideoGameWebAssembly/VideoGameWebAssembly/Controllers/VideoGamesController.cs
@@ -21,5 +21,55 @@ namespace VideoGameWebAssembly.Controllers
         {
             return await _context.VideoGames.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VideoGame>> GetGameByIdAsync(int id)
+        {
+            var result = await _context.VideoGames.FindAsync(id);
+
+            if (result == null) return NotFound("Game not found");
+
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddGameAsync(VideoGame newGame)
+        {
+            _context.Add(newGame);
+
+            await _context.SaveChangesAsync();
+            return Ok(newGame);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<VideoGame>> UpdateGameAsync(int id, VideoGame updatedGame)
+        {
+            var dbGame = await _context.VideoGames.FindAsync(id);
+            if (dbGame == null) 
+                return NotFound("Game not found");
+
+            dbGame.Title = updatedGame.Title;
+            dbGame.Publisher = updatedGame.Publisher;
+            dbGame.ReleaseYear = updatedGame.ReleaseYear;
+
+            await _context.SaveChangesAsync();
+            return Ok(dbGame);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteGameAsync(int id)
+        {
+            var result = await _context.VideoGames.FindAsync(id);
+
+            if (result == null) 
+                return NotFound("Game not found");
+
+            _context.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+
+        }
     }
 }
